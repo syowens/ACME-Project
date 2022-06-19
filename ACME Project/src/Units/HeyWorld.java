55c98e12ad9181749215b733fe6c47beb03a2d7f
Stacy Owens <s.shonn.o16@gmail.com>
2022-06-19 16:49:01 -0400

    Implemented heyNow

---
+++ ACME Project/src/Units/HeyWorld.java
@@ -0,0 +1,14 @@
+package Units;
+
+public class HeyWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	
+	public static void heyNow() {
+		String Msg = "Hey now";
+		System.out.println(Msg);
+	}
+
+}
